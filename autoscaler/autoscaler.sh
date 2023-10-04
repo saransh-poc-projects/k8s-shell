6013951c491dd1b3819dbf24402b2ff65c2b741b
Saransh Bhatt <saranshbhatt.nmims@gmail.com>
2023-10-04 11:21:22 +0530

    Update autoscaler.sh
    added scaleDown & scaleUp execution

--- autoscaler/autoscaler.sh
+++ autoscaler/autoscaler.sh
@@ -21,22 +21,33 @@ autoscaler(){
             # read values for each namespace from json input
             namespace_data=$(jq '.'"$clusters"'.'"$j" ${input_file})
             namespace_name=$(echo $namespace_data | jq -r '.namespace')
+            echo "Namespace: $namespace_name"
             scaleUpReplica=$(echo $namespace_data | jq -r '.max_replicas')      
             scaleDownReplica=$(echo $namespace_data | jq -r '.min_replicas')    
             #echo $namespace_data
             if [[$execute_scale_up]]; then
                 for deploy in $(kubectl get deploy -n $namespace_name | awk '{print $1}' | tail -n +2)
                 do
-                    echo "kubectl scale $deploy --replicas=$scaleUpReplica" >> $scaleUp_output_file
-                done                
+                    echo "kubectl scale $deploy --replicas=$scaleUpReplica -n $namespace_name" >> $scaleUp_output_file
+                done
+                chmod +x "$scaleUp_output_file"
+                echo "Executing Scale Up Action for deployments in namespace $namespace_name"
+                echo "Executing: $scaleUp_output_file ..."
+                #echo $scaleUp_output_file
+                ./"$scaleUp_output_file"
             elif [[$execute_scale_down]]; then            
                 for deploy in $(kubectl get deploy -n $namespace_name | awk '{print $1}' | tail -n +2)
                 do
-                    echo "kubectl scale $deploy --replicas=$scaleDownReplica" >> $scaleDown_output_file
+                    echo "kubectl scale $deploy --replicas=$scaleDownReplica -n $namespace_name" >> $scaleDown_output_file
                 done
+                chmod +x "$scaleDown_output_file"
+                echo "Executing Scale Down Action for deployments in namespace $namespace_name"
+                echo "Executing: $scaleDown_output_file ..."
+                #echo $scaleDown_output_file
+                ./"$scaleDown_output_file"
             else
                 echo "No scaling action enabled $namespace_name"
             fi  
-        done
+        done    
     done
 }
